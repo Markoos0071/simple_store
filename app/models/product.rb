ce7982243b2112ae95fa623d566e1dc1601a010b
Markus <mareme100@gmail.com>
2023-10-10 18:25:12 -0500

    Added products with faker

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
-class Product < ApplicationRecord
+class Product < ActiveRecord::Base
   validates :title, presence: true
   validates :price, presence: true
   validates :stock_quantity, presence: true
