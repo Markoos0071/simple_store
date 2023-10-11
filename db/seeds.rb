0ab2b5cf9691090160454a333ee8b96e315dbb42
Markus <mareme100@gmail.com>
2023-10-10 19:05:43 -0500

    Pulled and populated products &categories from CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "csv"
 require_relative '../config/environment'
 
-676.times do
-  product = Product.create(title: Faker::Commerce.product_name, price: Faker::Commerce.price,
-                            stock_quantity: Faker::Number.number(digits: 3))
+#676.times do
+#  product = Product.create(title: Faker::Commerce.product_name, price: Faker::Commerce.price,
+#                            stock_quantity: Faker::Number.number(digits: 3))
+#end
+
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |row|
+
+  category = Category.find_or_create_by(name: row['category'])
+
+  product = category.products.create(title: row['name'],
+                                      description: row['description'],
+                                      price: row['price'],
+                                      stock_quantity: row['stock quantity'])
+
 end
